557897671b31174eec9741f4c15add03bcadc0d3
Arnout Quint <arnoutpp@gmail.com>
2020-11-02 00:25:10 +0100

    created method to set and retrieve parameters in Route

--- arnode/Routing/Route.ts
+++ arnode/Routing/Route.ts
@@ -3,11 +3,14 @@ export interface IRoute {
     getPath(): string;
     setCallback(callback: Function): void;
     getCallback(): Function;
+    setParameter(parameterKey: string, parameterValue: string): void;
+    getParameters(): { [key: string]: string };
 }
 
 export class Route implements IRoute {
     private path: string;
     private callback: Function;
+    private parameters: { [key: string]: string };
 
     public setPath(path: string): void {
         this.path = path;
@@ -23,6 +26,14 @@ export class Route implements IRoute {
 
     public getCallback(): Function {
         return this.callback;
+    }
+
+    public setParameter(parameterKey: string, parameterValue: string): void {
+        this.parameters[parameterKey] = parameterValue;
+    }
+
+    public getParameters(): { [key: string]: string } {
+        return this.parameters;
     }
 
 }
