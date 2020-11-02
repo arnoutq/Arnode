0962929ab77b10c61b02b9f51f26b3ba7d4bc29e
Arnout Quint <arnoutpp@gmail.com>
2020-11-02 19:47:39 +0100

    created Response class with html response

---
+++ arnode/http/Response.ts
@@ -0,0 +1,20 @@
+import {ServerResponse} from "http";
+
+export interface IResponse {
+    html(htmlCode: string): void;
+}
+
+export class Response implements IResponse {
+    private response: ServerResponse;
+
+    constructor(response: ServerResponse) {
+        this.response = response;
+    }
+
+    public html(htmlCode: string): void {
+        const res = this.response;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(htmlCode);
+        res.end();
+    }
+}
