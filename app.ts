44181303cfb451352164b9a0c806f9645daece2f
Arnout Quint <arnoutpp@gmail.com>
2020-11-03 21:14:58 +0100

    added response to POST example

--- app.ts
+++ app.ts
@@ -9,7 +9,7 @@ const app = new Application(new Router(new RouteMatch()));
 app.get("/", (req: Request, res: Response)=> {
     res.html("<form method='POST' action='/test'><input name='testform'><input name='testform2'><button type='submit'>Submit</button></form>");
 });
-app.post("/test", (req: Request)=> { console.log(req.body.testform) });
+app.post("/test", (req: Request, res: Response)=> { res.json({value: req.body.testform}) });
 app.get("/middleware", ()=> { console.log("test") }).middleware(() => { console.log("middleware") });
 
 app.listen(8080);
